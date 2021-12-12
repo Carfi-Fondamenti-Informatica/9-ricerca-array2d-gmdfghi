cc7ff24f75b051178babc8b358193983149935c0
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-12 19:41:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int pos;
+    char a[10][20], b[1][20];
+
+for(int i=0;i<10;i++){
+    cin>>a[i];
+}
+cin>> b[0];
+
+if (ricerca(a,b,pos)){
+    cout<<pos<<endl;
+}else{
+    cout<<"non presente"<<endl;
+}
+    return 0;
 }
