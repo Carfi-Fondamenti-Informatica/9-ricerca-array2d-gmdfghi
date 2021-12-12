733b0dc47cfa394aeb4d20d751e68337bc74edee
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-12 19:56:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -7,7 +7,7 @@ bool ricerca (char lista [10][20], char nome[1][20], int &pos){
         if (strcmp (lista [i], nome) == 0) {
             pos = i;
             return true;
-        };
+        }
     }
     return false;
 }
