5a1c383f5952028807b29aa1496a193c778bf5a7
gmdfghi <91668752+gmdfghi@users.noreply.github.com>
2021-12-12 19:41:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+#include <cstring>
+
+bool ricerca (char nomi [10][20], char nome[1][20], int &ros){
+
+    for (int i = 0; i < 10; i++) {
+        if (strcmp(nomi[i], nome) == 0) {
+            pos = i;
+            return true;
+        };
+    }
+    return false;
+}
 
